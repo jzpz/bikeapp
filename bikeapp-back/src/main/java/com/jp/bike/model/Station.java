b9c67acfc5c2e04fc20862519fbf3a5af6bd6ac0
bgj424 <bgj424@myy.haaga-helia.fi>
2022-12-09 22:12:42 +0200

    Fix indentation

--- bikeapp-back/src/main/java/com/jp/bike/model/Station.java
+++ bikeapp-back/src/main/java/com/jp/bike/model/Station.java
@@ -1,102 +1,100 @@
-package com.jp.bike.model;
+	package com.jp.bike.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+	import jakarta.persistence.Column;
+	import jakarta.persistence.Entity;
+	import jakarta.persistence.Id;
+	import jakarta.persistence.Table;
 
-@Entity
-@Table(name = "bikeapp_stations")
-public class Station {
+	@Entity
+	@Table(name = "bikeapp_stations")
+	public class Station {
 
 	private @Id int fid;
 	private int id;
 	private String nameLocaleFi;
 	private String nameLocaleSe;
-    private String nameLocaleEn;
+	private String nameLocaleEn;
 	private String addressLocaleFi;
 	private String addressLocaleSe;
-    private String cityLocaleFi;
+	private String cityLocaleFi;
 	private String cityLocaleSe;
-    private String operator;
+	private String operator;
 	private String capacity;
-    @Column(name = "coordinate_x")
-    private Long coordinateX;
-    @Column(name = "coordinate_y")
+	@Column(name = "coordinate_x")
+	private Long coordinateX;
+	@Column(name = "coordinate_y")
 	private Long coordinateY;
-    
-    public Station() {}
 
-    public Station(int fid, int id, String nameLocaleFi, String nameLocaleSe, String nameLocaleEn, String addressLocaleFi,
-            String addressLocaleSe, String cityLocaleFi, String cityLocaleSe, String operator, String capacity,
-            Long coordinateX, Long coordinateY) {
-        this.fid = fid;
-        this.id = id;
-        this.nameLocaleFi = nameLocaleFi;
-        this.nameLocaleSe = nameLocaleSe;
-        this.nameLocaleEn = nameLocaleEn;
-        this.addressLocaleFi = addressLocaleFi;
-        this.addressLocaleSe = addressLocaleSe;
-        this.cityLocaleFi = cityLocaleFi;
-        this.cityLocaleSe = cityLocaleSe;
-        this.operator = operator;
-        this.capacity = capacity;
-        this.coordinateX = coordinateX;
-        this.coordinateY = coordinateY;
-    }
+	public Station() {}
 
-    public int getFid() {
-        return fid;
-    }
+	public Station(int fid, int id, String nameLocaleFi, String nameLocaleSe, String nameLocaleEn, String addressLocaleFi,
+			String addressLocaleSe, String cityLocaleFi, String cityLocaleSe, String operator, String capacity,
+			Long coordinateX, Long coordinateY) {
+		this.fid = fid;
+		this.id = id;
+		this.nameLocaleFi = nameLocaleFi;
+		this.nameLocaleSe = nameLocaleSe;
+		this.nameLocaleEn = nameLocaleEn;
+		this.addressLocaleFi = addressLocaleFi;
+		this.addressLocaleSe = addressLocaleSe;
+		this.cityLocaleFi = cityLocaleFi;
+		this.cityLocaleSe = cityLocaleSe;
+		this.operator = operator;
+		this.capacity = capacity;
+		this.coordinateX = coordinateX;
+		this.coordinateY = coordinateY;
+	}
 
-    public int getId() {
-        return id;
-    }
+	public int getFid() {
+		return fid;
+	}
 
-    public String getNameLocaleFi() {
-        return nameLocaleFi;
-    }
+	public int getId() {
+		return id;
+	}
 
-    public String getNameLocaleSe() {
-        return nameLocaleSe;
-    }
+	public String getNameLocaleFi() {
+		return nameLocaleFi;
+	}
 
-    public String getNameLocaleEn() {
-        return nameLocaleEn;
-    }
+	public String getNameLocaleSe() {
+		return nameLocaleSe;
+	}
 
-    public String getAddressLocaleFi() {
-        return addressLocaleFi;
-    }
+	public String getNameLocaleEn() {
+		return nameLocaleEn;
+	}
 
-    public String getAddressLocaleSe() {
-        return addressLocaleSe;
-    }
+	public String getAddressLocaleFi() {
+		return addressLocaleFi;
+	}
 
-    public String getCityLocaleFi() {
-        return cityLocaleFi;
-    }
+	public String getAddressLocaleSe() {
+		return addressLocaleSe;
+	}
 
-    public String getCityLocaleSe() {
-        return cityLocaleSe;
-    }
+	public String getCityLocaleFi() {
+		return cityLocaleFi;
+	}
 
-    public String getOperator() {
-        return operator;
-    }
+	public String getCityLocaleSe() {
+		return cityLocaleSe;
+	}
 
-    public String getCapacity() {
-        return capacity;
-    }
+	public String getOperator() {
+		return operator;
+	}
 
-    public Long getCoordinateX() {
-        return coordinateX;
-    }
+	public String getCapacity() {
+		return capacity;
+	}
 
-    public Long getCoordinateY() {
-        return coordinateY;
-    }
+	public Long getCoordinateX() {
+		return coordinateX;
+	}
 
-    
+	public Long getCoordinateY() {
+		return coordinateY;
+	}
 
 }
