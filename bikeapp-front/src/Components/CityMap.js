10cb2ad5f6961a5de9f351130d085c1ebc861fdd
bgj424 <bgj424@myy.haaga-helia.fi>
2022-12-15 12:21:39 +0200

    Cleaned map-related code

--- bikeapp-front/src/Components/CityMap.js
+++ bikeapp-front/src/Components/CityMap.js
@@ -4,15 +4,6 @@ import "../app.css"
 
 export default function CityMap({stations, departureStation, returnStation, 
     setDepartureStation, setReturnStation, currentSelectedStation, setCurrentSelectedStation}) {
-    
-    function isDepartureOrReturnStation(station) {
-        if(departureStation?.id === station.id) {
-            return true
-        } else if(returnStation?.id === station.id) {
-            return true
-        }
-        return false
-    }
 
     function markerColor(station) {
         if(departureStation?.id === station.id) {
@@ -23,6 +14,18 @@ export default function CityMap({stations, departureStation, returnStation,
         return null
     }
 
+    function selectStation(station = null) {
+        if(station) {
+            setCurrentSelectedStation(station)
+            setDepartureStation(station)
+            setReturnStation(station)
+        } else {
+            setCurrentSelectedStation([])
+            setDepartureStation([])
+            setReturnStation([])
+        }
+    }
+
     return (
         <Map 
             height={1000}
@@ -30,31 +33,25 @@ export default function CityMap({stations, departureStation, returnStation,
             minZoom={11} // Max zoom out distance
             center={[60.21, 24.95]} // Default location to Helsinki
             onClick={() => { // Remove marker highlights
-                setCurrentSelectedStation([])
-                setDepartureStation([])
-                setReturnStation([])
+                selectStation()
             }}
             className="city-map"
         >
             {stations.map((station) => { // Get all stations and mark them
                 return(
                     <Marker 
-                        width={isDepartureOrReturnStation(station) ? 50 : 30} // Make current station larger in map
+                        width={markerColor(station) ? 50 : 30} // Make current station larger in map
                         anchor={[station.coordinateY, station.coordinateX]} 
                         key={station.id}
                         onClick={() => {
                             if(currentSelectedStation.id === station.id) {
-                                setCurrentSelectedStation([])
-                                setDepartureStation([])
-                                setReturnStation([])
+                                selectStation()
                             } else {
-                                setCurrentSelectedStation(station)
-                                setDepartureStation(station)
-                                setReturnStation(station)
+                                selectStation(station)
                             }
                         }}
                         color={markerColor(station)} // Mark current station as red
-                        className={isDepartureOrReturnStation(station) ? "active" : ""} // Add class to current station marker
+                        className={markerColor(station)  ? "active" : ""} // Add class to current station marker
                         title={station.nameLocaleFi}
                     />
                 )
