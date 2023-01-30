d7f2a1eaaffcf38079185e207445f8e46636e0af
bgj424 <bgj424@myy.haaga-helia.fi>
2023-01-30 12:28:37 +0200

    fixed indentation on chunklistener

--- bikeapp-back/src/main/java/com/jp/bike/batch/listener/ImportChunkListener.java
+++ bikeapp-back/src/main/java/com/jp/bike/batch/listener/ImportChunkListener.java
@@ -6,16 +6,16 @@ import org.springframework.batch.core.scope.context.ChunkContext;
 // Print info after each batch chunk
 public class ImportChunkListener implements ChunkListener {
 
-    @Override
-    public void afterChunk(ChunkContext ccontext) {
-        System.out.println("Chunk complete - Added rows: " +
-            ccontext.getStepContext()
-                .getStepExecution()
-                .getWriteCount() + ", " +
-            "Skipped rows: " + 
-            ccontext.getStepContext()
-                .getStepExecution()
-                .getFilterCount());
-    }
-    
+	@Override
+	public void afterChunk(ChunkContext ccontext) {
+		System.out.println("Chunk complete - Added rows: " +
+			ccontext.getStepContext()
+				.getStepExecution()
+				.getWriteCount() + ", " +
+			"Skipped rows: " + 
+			ccontext.getStepContext()
+				.getStepExecution()
+				.getFilterCount());
+	}
+	
 }
